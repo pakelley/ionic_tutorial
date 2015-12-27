2122cac0d785f4e9909edac5db3db7ddb7bdb5f8
Patrick Kelley <patrick@the-kelleys.net>
2015-12-26 20:08:37 -0800

    Added Search Feature

--- www/js/services.js
+++ www/js/services.js
@@ -382,4 +382,33 @@ angular.module('ionic_udemy.services', [])
   };
 })
 
+.factory('searchService', function($q, $http) {
+
+  return {
+
+    search: function(query) {
+
+      var deferred = $q.defer(),
+
+      url = 'https://s.yimg.com/aq/autoc?query=' + query + '&region=CA&lang=en-CA&callback=YAHOO.util.ScriptNodeDataSource.callbacks';
+
+      YAHOO = window.YAHOO = {
+        util: {
+          ScriptNodeDataSource: {}
+        }
+      };
+
+      YAHOO.util.ScriptNodeDataSource.callbacks = function(data) {
+        var jsonData = data.ResultSet.Result;
+        deferred.resolve(jsonData);
+      };
+
+      $http.jsonp(url)
+        .then(YAHOO.util.ScriptNodeDataSource.callbacks);
+
+      return deferred.promise;
+    }
+  };
+})
+
 ;
